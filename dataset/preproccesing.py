456a9e276a68637cd380dd0c11a8514dd7d2bca0
Rishav Bhagat <rishavb123@gmail.com>
2020-05-03 17:19:33 -0400

    revamped entire load_data function with tons of customization

--- dataset/preproccesing.py
+++ dataset/preproccesing.py
@@ -1,45 +1,50 @@
 import os
 
+import time
+import datetime
 import numpy as np
 import cv2
 
-def load_data(image_dir):
+def load_data(image_dir, progress_bar_width=80, progress_char='+', empty_char='-', should_log=True, load_color=True, load_gray=True):
     faces = []
     gray = []
 
-    for fname in os.listdir(image_dir + '/color/'):
-        faces.append(cv2.imread(image_dir + "/color/" + fname))
-        g = cv2.imread(image_dir + "/grayscale/" + fname)
-        gcopy = []
-        for i in range(len(g)):
-            gcopy.append([])
-            for j in range(len(g[i])):
-                gcopy[i].append(g[i][j][0])
-        gray.append(gcopy)
+    ls = os.listdir(image_dir + '/color/')
+    l = len(ls)
 
-    return np.array(faces, float), np.array(gray, float)
+    def log(*args, **kwargs):
+        if should_log:
+            print(*args, **kwargs)
 
-def load_color_data(image_dir):
-    faces = []
+    log('Loading data from', '"' + image_dir + '"', '. . .')
 
-    for fname in os.listdir(image_dir + '/color/'):
-        faces.append(cv2.imread(image_dir + "/color/" + fname))
+    cur_time = time.time()
 
-    return np.array(faces, float)
+    for i in range(l):
+        w = i * progress_bar_width // l
+        s = int(time.time() - cur_time)
+        log('[' + progress_char * w + empty_char * (progress_bar_width - w) + ']', i, '/', l, datetime.timedelta(seconds=s), end='\r')
 
-def load_gray_data(image_dir):
-    gray = []
-
-    for fname in os.listdir(image_dir + '/color/'):
-        g = cv2.imread(image_dir + "/grayscale/" + fname)
-        gcopy = []
-        for i in range(len(g)):
-            gcopy.append([])
-            for j in range(len(g[i])):
-                gcopy[i].append(g[i][j][0])
-        gray.append(gcopy)
-
-    return np.array(gray, float)
+        fname = ls[i]
+        if load_color:
+            faces.append(cv2.imread(image_dir + "/color/" + fname))
+        if load_gray:
+            g = cv2.imread(image_dir + "/grayscale/" + fname)
+            gcopy = []
+            for i in range(len(g)):
+                gcopy.append([])
+                for j in range(len(g[i])):
+                    gcopy[i].append(g[i][j][0])
+            gray.append(gcopy)
+    log('[' + progress_char * progress_bar_width + ']', l, '/', l)
+    log('Finished loading data - Took', int(time.time() - cur_time), 'seconds')
+    if load_color and load_gray:
+        return np.array(faces, float), np.array(gray, float)
+    elif load_color:
+        return np.array(faces, float)
+    elif load_gray:
+        return np.array(gray, float)
+    return np.array([], float)
 
 def normalize(faces, output_range=(-1, 1), input_range=(0, 255)):
     return (faces - input_range[0]) * (output_range[1] - output_range[0]) / (input_range[1] - input_range[0]) + output_range[0]
