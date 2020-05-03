56a49841a3b88bd020cbff74058cfb650924bec5
Rishav Bhagat <rishavb123@gmail.com>
2020-05-03 15:49:27 -0400

    removed test code

--- dataset/preproccesing.py
+++ dataset/preproccesing.py
@@ -19,10 +19,5 @@ def load_data():
 
     return np.array(faces, float), np.array(gray, float)
 
-def normalize(faces, output_range=(0, 1), input_range=(0, 255)):
+def normalize(faces, output_range=(-1, 1), input_range=(0, 255)):
     return (faces - input_range[0]) * (output_range[1] - output_range[0]) / (input_range[1] - input_range[0]) + output_range[0]
-
-faces, gray = load_data()
-faces = normalize(faces, output_range=(-1, 1))
-gray = normalize(gray, output_range=(-1, 1))
-print(faces.shape, gray.shape)
