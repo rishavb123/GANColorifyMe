ae8a7fac5aaf8b49cc1945082050d7a0df3bc680
Rishav Bhagat <rishavb123@gmail.com>
2020-05-03 18:22:43 -0400

    created config variables

---
+++ gan/config.py
@@ -0,0 +1,16 @@
+update_data_file = False
+data_file = './data/gray.npy'
+
+BUFFER_SIZE = 100000
+BATCH_SIZE = 256
+
+EPOCHS = 100
+NOISE_DIM = 100
+
+LEARNING_RATE = 1e-4
+
+NUM_EXAMPLES_TO_GENERATE = 16
+
+checkpoint_frequency = 20
+checkpoint_dir = './training_checkpoints'
+log_dir = './logs'
