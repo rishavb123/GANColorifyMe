6b48f6eb0e55791030af86100cd9666d691dd328
Rishav Bhagat <rishavb123@gmail.com>
2020-05-04 18:55:06 -0400

    added more config for view script including rate of change and stability

--- view.py
+++ view.py
@@ -17,7 +17,8 @@ from gan.model_path import gen_path
 from colorify.model_path import path
 
 rand_start = False
-r = 0.9
+r = 0.1
+stability = 1
 
 capture = cv2.VideoCapture(0)
 
@@ -27,6 +28,8 @@ generator = tf.keras.models.load_model(gen_path.replace('.', './gan'))
 colorifier = tf.keras.models.load_model(path.replace('.', './colorify'))
 
 noise = tf.random.normal([1, 100]) if rand_start else np.load('./gan/inputs/best_noise_input.npy')
+count = 0
+approach = tf.random.normal([1, 100])
 
 while True:
     cur_time = time.time()
@@ -36,7 +39,11 @@ while True:
     for face in faces:
         x, y, w, h = face
 
-        noise = r * noise + (1 - r) * tf.random.normal([1, 100])
+        noise = (1 - r) * noise + r * approach
+        count += 1
+        if count >= stability:
+            count = 0
+            approach = tf.random.normal([1, 100])
 
         gen_img = colorifier(generator(noise, training=False), training=False)
         gen_img = normalize(gen_img, input_range=(-1, 1), output_range=(0, 255))
